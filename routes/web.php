2ba4b68d20b7cbc4d2b3d1b3c56e65cb6755496c
Noora Aulia H <nooraaulia20h@gmail.com>
2025-09-22 20:01:28 +0700

    fix(routes):remove duplicate /home route to restore login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductDummyController;
@@ -8,12 +7,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
-
-Route::get('/home', [ProductDummyController::class, 'index']);
-return view('home');
+Route::get('/home', [ProductDummyController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -24,9 +18,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
 
 require __DIR__ . '/auth.php';
