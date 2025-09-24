9c0cd5a6127b1e139b83fb02db914390e70aef97
Noora Aulia H <nooraaulia20h@gmail.com>
2025-09-24 10:58:12 +0700

    feat: Add user dropdown to navbar and create profile edit page

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 text-[var(--dark-brown)] bg-[var(--dark-gold)]'])
 
 @php
 $alignmentClasses = match ($align) {
